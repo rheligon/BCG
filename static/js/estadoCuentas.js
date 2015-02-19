82661610df48346433880dfd03b110095aa79135
Roberto Heligon <rheligon@gmail.com>
2015-02-19 12:09:45 -0430

    agregado de form edo cuentas, rellenado con ajax
    se completo el ajax para llenar con procesados y cargados, ademas para
    que llene el form.. la parte de eliminar un edc esta por la mitad

--- static/js/estadoCuentas.js
+++ static/js/estadoCuentas.js
@@ -3,6 +3,43 @@ var t_conta = iniciar_tabla(idioma_tr,'conta');
 var t_corr = iniciar_tabla(idioma_tr, 'corr');
 var csrftoken = $.cookie('csrftoken');
 
+$('#Cuenta-sel').change(function() {
+  var cuentaId = $('#Cuenta-sel').val();
+
+  if (cuentaId>=0){
+    var sel = $('#opt-'+cuentaId);
+    var moneda = sel.attr('moneda');
+
+    $('#cta_nostro').val(sel.attr('cn'));
+    $('#cta_vostro').val(sel.attr('cv'));
+    
+    $('#sf-ccon-mon').html(moneda);
+    $('#sf-ccor-mon').html(moneda);
+    $('#sf-pcon-mon').html(moneda);
+    $('#sf-pcor-mon').html(moneda);
+
+    t_conta.clear().draw();
+    t_corr.clear().draw();
+    est_cuenta(cuentaId);
+  }
+});
+
+//Mostrar Detalle al hacer click en código de banco
+$('.table').on('click','a[type=edc]', function(event) {
+    event.preventDefault();
+    var a_modo = $(this).attr("modo");
+    var a_cod = $(this).attr("id");
+    $("#elim-data").attr("modo", a_modo);
+    $("#elim-data").attr("cod", a_cod);
+});
+
+
+function reiniciar_tablas(){
+    t_conta.clear().draw();
+    t_corr.clear().draw();
+    return true;
+}
+
 function iniciar_tabla(idioma,origen){
 
     if (idioma==="es"){
@@ -50,12 +87,71 @@ function est_cuenta(cuentaId){
         success: function(data){
             var carg = 0;
             var proc = 1;
-
+            var ult_edc_conc = 0;
+            var ult_edc_conp = 0;
+            var ult_edc_corc = 0;
+            var ult_edc_corp = 0;
+            var ult_conc_existe = false;
+            var ult_corc_existe = false;
+            var ult_conp_existe = false;
+            var ult_corp_existe = false;
             var json_data = jQuery.parseJSON(data)
+
             if (json_data[carg].length<1){
                 alert("cargados esta vacio")
             }else{
 
+                var bank = json_data[carg][0].fields.cuenta_idcuenta;
+                var bankcod = $('#opt-'+bank).html();
+
+                for (var i = 0; i < json_data[carg].length; i++) {
+
+                    var date_ini = new Date(json_data[carg][i].fields.fecha_inicial)
+                    var date_fin = new Date(json_data[carg][i].fields.fecha_final)
+                    var cod = json_data[carg][i].fields.codigo
+
+                    var td1 = '<td>'+ '<a id="'+ cod +'" type="edc" modo="carg">' + bankcod + '</a></td>';
+                    var td2 = '<td> cargado </td>';
+                    var td3 = '<td>' + json_data[carg][i].fields.codigo + '</td>';
+                    var td4 = '<td>' + json_data[carg][i].fields.pagina + '</td>';
+                    var td5 = '<td>' + json_data[carg][i].fields.m_finicial + ' '+ json_data[carg][i].fields.c_dinicial + ' '+ date_ini.toLocaleDateString() + ' ' + json_data[carg][i].fields.balance_inicial +'</td>';
+                    var td6 = '<td>' + json_data[carg][i].fields.m_ffinal + ' '+ json_data[carg][i].fields.c_dfinal + ' '+ date_fin.toLocaleDateString() + ' ' + json_data[carg][i].fields.balance_final +'</td>';
+
+
+
+                    if (json_data[carg][i].fields.origen == "L"){
+                        $('#table-conta > tbody').append('<tr id ="tr-con-'+cod+'"></tr>');
+                        var jRow = $("#tr-con-"+cod).append(td1,td2,td3,td4,td5,td6);
+                        t_conta.row.add(jRow);
+                        
+                        if (!ult_conc_existe){
+                            ult_edc_corc = i;
+                            ult_conc_existe = true;
+                        }
+                        
+                        if (json_data[carg][ult_edc_conp].fields.codigo < json_data[carg][i].fields.codigo){
+                            ult_edc_conc = i;
+                        }
+
+                        
+
+                    }else{
+                        $('#table-corr > tbody').append('<tr id ="tr-cor-'+cod+'"></tr>');
+                        var jRow = $("#tr-cor-"+cod).append(td1,td2,td3,td4,td5,td6);
+                        t_corr.row.add(jRow);
+
+                        if (!ult_corc_existe){
+                            ult_edc_corc = i;
+                            ult_corc_existe = true;
+                        }
+                        if (json_data[carg][ult_edc_corp].fields.codigo < json_data[carg][i].fields.codigo){
+                            ult_edc_corc = i;
+                        }
+                    }
+                }
+                t_conta.draw();
+                t_corr.draw();
+                alert("cargados agregados satisfactoriamente.");
             }
 
             if (json_data[proc].length<1){
@@ -71,7 +167,7 @@ function est_cuenta(cuentaId){
                     var date_fin = new Date(json_data[proc][i].fields.fecha_final)
                     var cod = json_data[proc][i].fields.codigo
 
-                    var td1 = '<td>'+ '<a id="'+ cod +'">' + bankcod + '</a></td>';
+                    var td1 = '<td>'+ '<a id="'+ cod +'" type="edc" modo="proc">' + bankcod + '</a></td>';
                     var td2 = '<td> procesado </td>';
                     var td3 = '<td>' + json_data[proc][i].fields.codigo + '</td>';
                     var td4 = '<td>' + json_data[proc][i].fields.pagina + '</td>';
@@ -81,16 +177,84 @@ function est_cuenta(cuentaId){
 
 
                     if (json_data[proc][i].fields.origen == "L"){
-                        $('#table-conta > tbody').append('<tr id ="tr-'+cod+'"></tr>');
-                        var jRow = $("#tr-"+cod).append(td1,td2,td3,td4,td5,td6);
-                        t_conta.row.add(jRow).draw();
+                        $('#table-conta > tbody').append('<tr id ="tr-con-'+cod+'"></tr>');
+                        var jRow = $("#tr-con-"+cod).append(td1,td2,td3,td4,td5,td6);
+                        t_conta.row.add(jRow);
+                        
+                        if (!ult_conp_existe){
+                            ult_edc_corp = i;
+                            ult_conp_existe = true;
+                        }
+                        
+                        if (json_data[proc][ult_edc_conp].fields.codigo < json_data[proc][i].fields.codigo){
+                            ult_edc_conp = i;
+                        }
+
+                        
+
                     }else{
-                        $('#table-corr > tbody').append('<tr id ="tr-'+cod+'"></tr>');
-                        var jRow = $("#tr-"+cod).append(td1,td2,td3,td4,td5,td6);
-                        t_corr.row.add(jRow).draw();
+                        $('#table-corr > tbody').append('<tr id ="tr-cor-'+cod+'"></tr>');
+                        var jRow = $("#tr-cor-"+cod).append(td1,td2,td3,td4,td5,td6);
+                        t_corr.row.add(jRow);
+
+                        if (!ult_corp_existe){
+                            ult_edc_corp = i;
+                            ult_corp_existe = true;
+                        }
+                        if (json_data[proc][ult_edc_corp].fields.codigo < json_data[proc][i].fields.codigo){
+                            ult_edc_corp = i;
+                        }
                     }
                 }
-                alert("agregados satisfactoriamente.");
+                t_conta.draw();
+                t_corr.draw();
+                alert("procesados agregados satisfactoriamente.");
+            }
+
+            if (ult_conc_existe || ult_conp_existe){
+                var fecha_conc = new Date(json_data[proc][ult_edc_conc].fields.fecha_final)
+                var fecha_conp = new Date(json_data[proc][ult_edc_conp].fields.fecha_final)
+
+                $('#edc-ccon').val(json_data[proc][ult_edc_conc].fields.codigo);
+                $('#f-ccon').val(fecha_conc.toLocaleDateString());
+                $('#sf-ccon-cd').html(json_data[proc][ult_edc_conc].fields.c_dfinal);
+                $('#sf-ccon').val(json_data[proc][ult_edc_conc].fields.balance_final);
+
+                $('#edc-pcon').val(json_data[proc][ult_edc_conp].fields.codigo);
+                $('#f-pcon').val(fecha_conp.toLocaleDateString());
+                $('#sf-pcon-cd').html(json_data[proc][ult_edc_conp].fields.c_dfinal);
+                $('#sf-pcon').val(json_data[proc][ult_edc_conp].fields.balance_final);
+                
+                if (!ult_conc_existe){
+                    $('#edc-ccon').val(json_data[proc][ult_edc_conp].fields.codigo);
+                    $('#f-ccon').val(fecha_conp.toLocaleDateString());
+                    $('#sf-ccon-cd').html(json_data[proc][ult_edc_conp].fields.c_dfinal);
+                    $('#sf-ccon').val(json_data[proc][ult_edc_conp].fields.balance_final); 
+                }
+
+            }
+            if (ult_corc_existe || ult_corp_existe){
+                var fecha_corc = new Date(json_data[proc][ult_edc_corc].fields.fecha_final)
+                var fecha_corp = new Date(json_data[proc][ult_edc_corp].fields.fecha_final)
+
+                $('#edc-ccor').val(json_data[proc][ult_edc_corc].fields.codigo);
+                $('#f-ccor').val(fecha_corc.toLocaleDateString());
+                $('#sf-ccor-cd').html(json_data[proc][ult_edc_corc].fields.c_dfinal);
+                $('#sf-ccor').val(json_data[proc][ult_edc_corc].fields.balance_final);
+
+                $('#edc-pcor').val(json_data[proc][ult_edc_corp].fields.codigo);
+                $('#f-pcor').val(fecha_corp.toLocaleDateString());
+                $('#sf-pcor-cd').html(json_data[proc][ult_edc_corp].fields.c_dfinal);
+                $('#sf-pcor').val(json_data[proc][ult_edc_corp].fields.balance_final);
+
+                if (!ult_corc_existe){
+                    $('#edc-ccor').val(json_data[proc][ult_edc_corp].fields.codigo);
+                    $('#f-ccor').val(fecha_corp.toLocaleDateString());
+                    $('#sf-ccor-cd').html(json_data[proc][ult_edc_corp].fields.c_dfinal);
+                    $('#sf-ccor').val(json_data[proc][ult_edc_corp].fields.balance_final);
+
+                }
+
             }
         },
         error: function(error){
@@ -104,3 +268,35 @@ function est_cuenta(cuentaId){
     return false;
 }
 
+//Eliminar Banco
+$('#delButton').on('click', function () {
+    var $btn;
+   
+    function del_edc(edcId, cop){
+        $.ajax({
+            type:"POST",
+            url: "/cuentas/estado",
+            data: {"cuentaid": -1, "edcid": edcId, "cop": cop},
+            success: function(data){
+                alert(data.msg);
+
+                if (data.elim){
+                    tabla.row($('#tr-'+ data.bancoid)).remove().draw();
+                    $(".banco-detalle").hide();
+                }
+
+                $btn.button('reset')
+            },
+            dataType:'json',
+            headers:{
+                'X-CSRFToken':csrftoken
+            }
+        });
+        return false;
+    }
+
+    if (confirm("Seguro que desea eliminar el banco?")){
+        $btn = $(this).button('loading')
+        del_edc($("#Id_banco").val());
+    }
+})
