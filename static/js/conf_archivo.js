842cf992d8c99cdc80ec61a2f965b4b9d7fc62d0
Roberto Heligon <rheligon@gmail.com>
2015-03-02 14:39:55 -0430

    Conf_Archivo terminado (Error CSRF corregido)
    El error era que se hacia submit del form, por lo que se uso
    "event.preventDefault()" para que el boton no hiciera submit sino q se
    hiciera mediante ajax

--- static/js/conf_archivo.js
+++ static/js/conf_archivo.js
@@ -180,6 +180,7 @@ $(function () {
 
 //Agregar Formato
 $('#addfa-submit').on('click', function () {
+    event.preventDefault();
     var $btn;
    
     function add_formarc(formCuenta, formNom, formCarsep, formTipo, formCampSel){
