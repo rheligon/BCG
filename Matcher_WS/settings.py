1991720d6b8d310cbf3ff23cd007e8df51b405b4
jotha41 <jotha41@gmail.com>
2015-10-14 14:08:48 -0400

    bla

--- Matcher_WS/settings.py
+++ Matcher_WS/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Matcher_WS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'sqlserver_ado',
-        'NAME': 'MatcherDSP',
+        'NAME': 'Matcher',
         'HOST': '190.168.1.128',
         'PORT': '1433',
         'USER': 'sa',
